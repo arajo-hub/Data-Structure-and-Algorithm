76469fa34c76f72286ad81fda9704b461467e921
arajo-hub <joara9566@naver.com>
2020-05-29 23:37:01 +0900

    Binary Search Tree

---
+++ 10. Binary Search Tree.py
@@ -0,0 +1,53 @@
+# Binary search Tree는 데이터를 빨리 찾기 위한 방법이다.
+# 이진트리형태로 되어있으므로 logN의 시간복잡도를 가지고, 최악의 경우에 N의 시간복잡도를 갖게 된다.
+
+class Node:
+    def __init__(self, val):
+        self.val=val
+        self.left=None
+        self.right=None
+
+def insert(node, val):
+
+    # 만약에 root가 비어있을 때
+    # node를 root로 만든다.
+
+    if node is None:
+        return Node(val)
+
+    # 만약 root의 val가 새로운 val보다 클 경우
+    if val<node.val:
+        node.left=insert(node.left, val)
+
+    # 만약 root의 val가 새로운 val보다 작을 경우
+    if val<node.val:
+        node.right=insert(node.right, val)
+
+    # root 리턴
+    return node
+
+# inorder
+
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+# 가장 작은 값 출력
+def minimum(node):
+    # 노드가 가장 왼쪽으로 갈 경우
+    while (node.left is not None):
+        node=node.left
+    return node.val
+
+root=None
+root=insert(root, 50)
+root=insert(root, 30)
+root=insert(root, 20)
+root=insert(root, 40)
+root=insert(root, 70)
+root=insert(root, 60)
+root=insert(root, 80)
+inorder(root)
+print(minimum(root))
